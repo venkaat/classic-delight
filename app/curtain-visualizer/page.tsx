5fadefedbe2812c0a678eab78ccb5be4233ea724
venkaat <vvvvkt@gmail.com>
2026-05-10 21:41:31 +0530

    fix visualizer page

--- app/curtain-visualizer/page.tsx
+++ app/curtain-visualizer/page.tsx
@@ -1,9 +1,9 @@
-import Header from "@/app/components/Header";
-import CurtainVisualizerExperience from "@/app/components/CurtainVisualizerExperience";
+import Header from "../components/Header";
+import CurtainVisualizerExperience from "../components/CurtainVisualizerExperience";
 
 export default function CurtainVisualizerPage() {
   return (
-    <main className="bg-black">
+    <main className="bg-black min-h-screen">
       <Header />
       <CurtainVisualizerExperience />
     </main>
