9af1053219e7d6b039987c8fe2032bda95dcf38a
venkaat <vvvvkt@gmail.com>
2026-05-12 00:29:19 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=AW-998359476"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-ads" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-998359476');
+  `}
+</Script>
         {children}
+        
       </body>
     </html>
   );
